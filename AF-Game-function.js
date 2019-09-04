101bb64421e9c32ae229ac02acf217ea2aab2966
Gooss <gooss@media-saturn.com>
2019-09-04 14:30:21 +0200

    First version created with html file, basic css and empty functions to be finished

---
+++ AF-Game-function.js
@@ -0,0 +1,36 @@
+//let downsAtStart = 100; //have to get reduced after every down until 0 -> game is over
+//let score = 0; //has to add 7 per side which scored
+
+//document.getElementById("MyTeamHuskies").style.left="100px";
+
+let myTeam = {
+    teamName: "Springfield Huskies",
+    passPlay: function(){
+
+    },
+    runPlay: function(){
+
+    },
+    passDefense: function(){
+
+    },
+    runDefense: function(){
+
+    },
+}
+
+let opponentTeam = {
+    teamName: "Queens Drunken Irish",
+    passPlay: function(){
+
+    },
+    runPlay: function(){
+
+    },
+    passDefense: function(){
+
+    },
+    runDefense: function(){
+
+    },
+}
